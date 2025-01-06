c41c290aab2ec2f1412ad800c2f4c3cc3993b1b5
jacobhenry96 <jacobkhenry2@gmail.com>
2025-01-05 20:45:00 -0600

    portfolio page designing and adding js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("Hello World");
+let project1 = document.querySelector(".project1");
+let project2 = document.querySelector(".project2");
+let project3 = document.querySelector(".project3");
+let project4 = document.querySelector(".project4");
+let project5 = document.querySelector(".project5");
+
+let portfolioImg = document.querySelector("#portfolio-img");
+
+
+project1.addEventListener('click', () => {
+    portfolioImg.src = "images/Screenshot 2024-10-31 221511.png"
+});
+project2.addEventListener('click', () => {
+    portfolioImg.src = "images/Screenshot 2024-10-31 221811.png"
+});
+project3.addEventListener('click', () => {
+    portfolioImg.src = "images/Screenshot 2024-10-31 221854.png"
+});
+project4.addEventListener('click', () => {
+    portfolioImg.src = "images/Screenshot 2024-10-31 221948.png"
+});
+project5.addEventListener('click', () => {
+    portfolioImg.src = "images/Screenshot 2024-11-10 220619.png"
+});
+
+
+
